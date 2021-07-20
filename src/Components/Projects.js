3b615664a19bb83308f8ed29be68b4b8da229b20
Onyekachi Godswill <omenukoonyekachi690@gmail.com>
2021-07-21 00:38:04 +0100

    working on home

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -14,7 +14,7 @@ const Projects = () => {
           </div>
         </div>
                      <div className="text-right">
-                       <a href="#!"><span>View all</span> </a>
+                       <a href="#!"><span>View all</span> <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M13.025 1l-2.847 2.828 6.176 6.176h-16.354v3.992h16.354l-6.176 6.176 2.847 2.828 10.975-11z"/></svg></a>
                      </div>
 
              <div className="d-flex justify-content-center flex-wrap">
