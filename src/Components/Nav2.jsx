16137bf1812bbacfe07202d41a6a674ad6d9857a
Onyekachi Godswill <omenukoonyekachi690@gmail.com>
2021-07-21 02:41:19 +0100

    working on route

---
+++ src/Components/Nav2.jsx
@@ -0,0 +1,78 @@
+import React, { useState } from "react";
+import { Link } from "react-scroll";
+import { NavLink } from "react-router-dom";
+
+const Nav2 = () => {
+  const [click, setClick] = useState(true);
+
+  function closeMenu() {
+    setClick(!click);
+  }
+
+  return (
+    <>
+      <nav className="navbar navbar-expand-lg navbar-light">
+        <div className="container">
+          <NavLink
+            className="navbar-brand text-white"
+            to="/"
+           
+          >
+            Godswill
+          </NavLink>
+          <button className="navbar-toggler" onClick={closeMenu}>
+            {click ? (
+              <i className="fas fa-bars text-white"></i>
+            ) : (
+              <i className="fas fa-times text-white"></i>
+            )}
+          </button>
+
+          <div
+            className={click ? "navbar-collapse collapse" : "navbar-collapse"}
+          >
+            <ul className="navbar-nav ml-auto">
+              <li className="nav-item">
+                <a
+                  className="nav-link"
+                  href="/#header"
+                 
+                  onClick={closeMenu}
+                >
+                  Home <span className="sr-only">(current)</span>
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="/#about" onClick={closeMenu}>
+                  About Me
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="/#service" onClick={closeMenu}>
+                  Services
+                </a>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="project" spy={true} offset={-80} onClick={closeMenu}>
+                  Projects
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="skill" spy={true} offset={-80} onClick={closeMenu}>
+                  Skills
+                </Link>
+              </li>
+              <li className="nav-item">
+              <Link className="nav-link" to="about" spy={true} offset={-80} onClick={closeMenu}>
+                  Contact
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+};
+
+export default Nav2;
