83e163f149a788c6085bdaec90e8aaa810cd1dc0
Onyekachi Godswill <omenukoonyekachi690@gmail.com>
2021-07-21 02:46:19 +0100

    working on route

--- src/Components/AllProject.js
+++ src/Components/AllProject.js
@@ -1,63 +1,7 @@
 const AllProject = () => {
     return (
-        <div className="container mt-5 mb-5 pt-5">
-            hello
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestiae culpa exercitationem alias, est dolorem optio delectus temporibus porro animi veritatis quaerat ut, consequuntur iste similique perspiciatis tenetur assumenda modi nostrum.
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestiae culpa exercitationem alias, est dolorem optio delectus temporibus porro animi veritatis quaerat ut, consequuntur iste similique perspiciatis tenetur assumenda modi nostrum.
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestiae culpa exercitationem alias, est dolorem optio delectus temporibus porro animi veritatis quaerat ut, consequuntur iste similique perspiciatis tenetur assumenda modi nostrum.
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestiae culpa exercitationem alias, est dolorem optio delectus temporibus porro animi veritatis quaerat ut, consequuntur iste similique perspiciatis tenetur assumenda modi nostrum.
-            hello
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestiae culpa exercitationem alias, est dolorem optio delectus temporibus porro animi veritatis quaerat ut, consequuntur iste similique perspiciatis tenetur assumenda modi nostrum.
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestiae culpa exercitationem alias, est dolorem optio delectus temporibus porro animi veritatis quaerat ut, consequuntur iste similique perspiciatis tenetur assumenda modi nostrum.
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestiae culpa exercitationem alias, est dolorem optio delectus temporibus porro animi veritatis quaerat ut, consequuntur iste similique perspiciatis tenetur assumenda modi nostrum.
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestiae culpa exercitationem alias, est dolorem optio delectus temporibus porro animi veritatis quaerat ut, consequuntur iste similique perspiciatis tenetur assumenda modi nostrum.
-            hello
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestiae culpa exercitationem alias, est dolorem optio delectus temporibus porro animi veritatis quaerat ut, consequuntur iste similique perspiciatis tenetur assumenda modi nostrum.
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestiae culpa exercitationem alias, est dolorem optio delectus temporibus porro animi veritatis quaerat ut, consequuntur iste similique perspiciatis tenetur assumenda modi nostrum.
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestiae culpa exercitationem alias, est dolorem optio delectus temporibus porro animi veritatis quaerat ut, consequuntur iste similique perspiciatis tenetur assumenda modi nostrum.
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestiae culpa exercitationem alias, est dolorem optio delectus temporibus porro animi veritatis quaerat ut, consequuntur iste similique perspiciatis tenetur assumenda modi nostrum.
-            hello
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestiae culpa exercitationem alias, est dolorem optio delectus temporibus porro animi veritatis quaerat ut, consequuntur iste similique perspiciatis tenetur assumenda modi nostrum.
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestiae culpa exercitationem alias, est dolorem optio delectus temporibus porro animi veritatis quaerat ut, consequuntur iste similique perspiciatis tenetur assumenda modi nostrum.
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestiae culpa exercitationem alias, est dolorem optio delectus temporibus porro animi veritatis quaerat ut, consequuntur iste similique perspiciatis tenetur assumenda modi nostrum.
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestiae culpa exercitationem alias, est dolorem optio delectus temporibus porro animi veritatis quaerat ut, consequuntur iste similique perspiciatis tenetur assumenda modi nostrum.
-            hello
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestiae culpa exercitationem alias, est dolorem optio delectus temporibus porro animi veritatis quaerat ut, consequuntur iste similique perspiciatis tenetur assumenda modi nostrum.
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestiae culpa exercitationem alias, est dolorem optio delectus temporibus porro animi veritatis quaerat ut, consequuntur iste similique perspiciatis tenetur assumenda modi nostrum.
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestiae culpa exercitationem alias, est dolorem optio delectus temporibus porro animi veritatis quaerat ut, consequuntur iste similique perspiciatis tenetur assumenda modi nostrum.
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestiae culpa exercitationem alias, est dolorem optio delectus temporibus porro animi veritatis quaerat ut, consequuntur iste similique perspiciatis tenetur assumenda modi nostrum.
-            hello
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestiae culpa exercitationem alias, est dolorem optio delectus temporibus porro animi veritatis quaerat ut, consequuntur iste similique perspiciatis tenetur assumenda modi nostrum.
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestiae culpa exercitationem alias, est dolorem optio delectus temporibus porro animi veritatis quaerat ut, consequuntur iste similique perspiciatis tenetur assumenda modi nostrum.
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestiae culpa exercitationem alias, est dolorem optio delectus temporibus porro animi veritatis quaerat ut, consequuntur iste similique perspiciatis tenetur assumenda modi nostrum.
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestiae culpa exercitationem alias, est dolorem optio delectus temporibus porro animi veritatis quaerat ut, consequuntur iste similique perspiciatis tenetur assumenda modi nostrum.
-            hello
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestiae culpa exercitationem alias, est dolorem optio delectus temporibus porro animi veritatis quaerat ut, consequuntur iste similique perspiciatis tenetur assumenda modi nostrum.
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestiae culpa exercitationem alias, est dolorem optio delectus temporibus porro animi veritatis quaerat ut, consequuntur iste similique perspiciatis tenetur assumenda modi nostrum.
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestiae culpa exercitationem alias, est dolorem optio delectus temporibus porro animi veritatis quaerat ut, consequuntur iste similique perspiciatis tenetur assumenda modi nostrum.
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestiae culpa exercitationem alias, est dolorem optio delectus temporibus porro animi veritatis quaerat ut, consequuntur iste similique perspiciatis tenetur assumenda modi nostrum.
-            hello
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestiae culpa exercitationem alias, est dolorem optio delectus temporibus porro animi veritatis quaerat ut, consequuntur iste similique perspiciatis tenetur assumenda modi nostrum.
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestiae culpa exercitationem alias, est dolorem optio delectus temporibus porro animi veritatis quaerat ut, consequuntur iste similique perspiciatis tenetur assumenda modi nostrum.
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestiae culpa exercitationem alias, est dolorem optio delectus temporibus porro animi veritatis quaerat ut, consequuntur iste similique perspiciatis tenetur assumenda modi nostrum.
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestiae culpa exercitationem alias, est dolorem optio delectus temporibus porro animi veritatis quaerat ut, consequuntur iste similique perspiciatis tenetur assumenda modi nostrum.
-            hello
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestiae culpa exercitationem alias, est dolorem optio delectus temporibus porro animi veritatis quaerat ut, consequuntur iste similique perspiciatis tenetur assumenda modi nostrum.
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestiae culpa exercitationem alias, est dolorem optio delectus temporibus porro animi veritatis quaerat ut, consequuntur iste similique perspiciatis tenetur assumenda modi nostrum.
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestiae culpa exercitationem alias, est dolorem optio delectus temporibus porro animi veritatis quaerat ut, consequuntur iste similique perspiciatis tenetur assumenda modi nostrum.
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestiae culpa exercitationem alias, est dolorem optio delectus temporibus porro animi veritatis quaerat ut, consequuntur iste similique perspiciatis tenetur assumenda modi nostrum.
-            hello
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestiae culpa exercitationem alias, est dolorem optio delectus temporibus porro animi veritatis quaerat ut, consequuntur iste similique perspiciatis tenetur assumenda modi nostrum.
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestiae culpa exercitationem alias, est dolorem optio delectus temporibus porro animi veritatis quaerat ut, consequuntur iste similique perspiciatis tenetur assumenda modi nostrum.
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestiae culpa exercitationem alias, est dolorem optio delectus temporibus porro animi veritatis quaerat ut, consequuntur iste similique perspiciatis tenetur assumenda modi nostrum.
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestiae culpa exercitationem alias, est dolorem optio delectus temporibus porro animi veritatis quaerat ut, consequuntur iste similique perspiciatis tenetur assumenda modi nostrum.
-            hello
-          <div id="td">
-          1234567 ipsum dolor sit amet consectetur, adipisicing elit. Molestiae culpa exercitationem alias, est dolorem optio delectus temporibus porro animi veritatis quaerat ut, consequuntur iste similique perspiciatis tenetur assumenda modi nostrum.
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestiae culpa exercitationem alias, est dolorem optio delectus temporibus porro animi veritatis quaerat ut, consequuntur iste similique perspiciatis tenetur assumenda modi nostrum.
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestiae culpa exercitationem alias, est dolorem optio delectus temporibus porro animi veritatis quaerat ut, consequuntur iste similique perspiciatis tenetur assumenda modi nostrum.
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestiae culpa exercitationem alias, est dolorem optio delectus temporibus porro animi veritatis quaerat ut, consequuntur iste similique perspiciatis tenetur assumenda modi nostrum.
-          </div>
+        <div className="container">
+             
         </div>
     )
 }
