06eb5ac4dc4379a9ee83a5f40401a111d8b9d6c6
Onyekachi Godswill <omenukoonyekachi690@gmail.com>
2021-07-21 02:13:52 +0100

    working on route

--- src/Components/Nav2.jsx
+++ src/Components/Nav2.jsx
@@ -1,9 +1,16 @@
+import { Link } from "react-router-dom";
+
 function Nav2() {
-    return (
-        <div>
-            hello
-        </div>
-    )
+  return (
+    <div className="nav2">
+      <div className="container">
+           <ul className="d-flex list-unstyled justify-content-around align-items-center py-2">
+               <Link className="nav2-link" to="/">Godswill</Link>
+               <Link className="nav2-link" to="/">HOME</Link>
+           </ul>
+          </div>
+    </div>
+  );
 }
 
-export default Nav2
+export default Nav2;
