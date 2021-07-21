a851e3c5832742c954e1a401625b1f2c99c598f8
Onyekachi Godswill <omenukoonyekachi690@gmail.com>
2021-07-21 02:55:55 +0100

    working on route

--- src/Components/AllProject.js
+++ src/Components/AllProject.js
@@ -1,7 +1,10 @@
 const AllProject = () => {
     return (
-        <div className="container">
-             
+        <div className="container work-pro">
+              <div className="text-center">
+                  <h1>Work in Progress..</h1>
+                  <p>Please come back after some time... Thanks!!</p>
+              </div>
         </div>
     )
 }
