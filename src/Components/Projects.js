4135ab14989fc765dfd1cd556f3473e2efb57ecd
Onyekachi Godswill <omenukoonyekachi690@gmail.com>
2021-07-22 05:39:43 +0100

    working on 404

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -4,6 +4,37 @@ import { projects } from "./projectData";
 
 const Projects = () => {
   // Main return
+
+  const team1 = projects.map((data) => {
+    const { title, image, desc, technology, siteLink, id } = project;
+    return (
+      <div className="project-box" key={id} style={{ margin: 13 }}>
+        <div className="project-imgBx">
+          <img src={image} alt="project" className="img-fluid" />
+        </div>
+        <div className="project-content">
+          <h2>{title}</h2>
+          <h3>
+            Technologies: <span>{technology}</span>
+          </h3>
+          <p className="text-left">{desc}</p>
+          <div className="d-flex justify-content-center mt-2 pt-4">
+            <a
+              href={siteLink}
+              className="project-btn"
+              target="blank"
+              rel="noopener noreferre"
+            >
+              View site
+            </a>
+          </div>
+        </div>
+      </div>
+    );
+  });
+  const selectedTeam = team1.slice(1, 5);
+
+
   return (
     <div className="project" id="project">
       <div className="container">
@@ -32,33 +63,7 @@ const Projects = () => {
         </div>
 
         <div className="d-flex justify-content-center flex-wrap">
-          {projects.map((project) => {
-            const { title, image, desc, technology, siteLink, id } = project;
-            return (
-              <div className="project-box" key={id} style={{ margin: 13 }}>
-                <div className="project-imgBx">
-                  <img src={image} alt="project" className="img-fluid" />
-                </div>
-                <div className="project-content">
-                  <h2>{title}</h2>
-                  <h3>
-                    Technologies: <span>{technology}</span>
-                  </h3>
-                  <p className="text-left">{desc}</p>
-                  <div className="d-flex justify-content-center mt-2 pt-4">
-                    <a
-                      href={siteLink}
-                      className="project-btn"
-                      target="blank"
-                      rel="noopener noreferre"
-                    >
-                      View site
-                    </a>
-                  </div>
-                </div>
-              </div>
-            );
-          })}
+           {}
         </div>
       </div>
     </div>
