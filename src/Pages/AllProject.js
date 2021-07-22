4b30d84cd3b00330f77aed74e877b6f9f14ce19e
Onyekachi Godswill <omenukoonyekachi690@gmail.com>
2021-07-22 06:13:07 +0100

    working on project

--- src/Pages/AllProject.js
+++ src/Pages/AllProject.js
@@ -1,25 +1,16 @@
 import { projects } from "../Components/projectData";
 const AllProject = () => {
-
-
   return (
-    <div className="container" style={{marginTop:  '7rem'}}>
+    <div className="container" style={{ marginTop: "7rem" }}>
       <div className="btns">
-        <button value="All">
-          All
-        </button>
-        <button value="Apple">
-          Iphone
-        </button>
-        <button value="Samsung">
-          Samsung
-        </button>
-        <button value="lg">
-          LG
-        </button>
+        <button value="All">All</button>
+        <button value="Apple">Iphone</button>
+        <button value="Samsung">Samsung</button>
+        <button value="lg">LG</button>
       </div>
 
-           
+
+      
     </div>
   );
 };
